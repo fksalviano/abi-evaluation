0ce4a92e32cf5b1661c66cadcce6f0b994ffb887
Fabio Salviano <fksalviano@gmail.com>
2025-03-21 16:38:28 -0300

    feat: add handlers logging

--- src/Ambev.DeveloperEvaluation.Application/Sales/DeleteSale/DeleteSaleHandler.cs
+++ src/Ambev.DeveloperEvaluation.Application/Sales/DeleteSale/DeleteSaleHandler.cs
@@ -1,5 +1,7 @@
+using System.Text.Json;
 using Ambev.DeveloperEvaluation.Domain.Repositories;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace Ambev.DeveloperEvaluation.Application.Sales.DeleteSale;
 
@@ -9,14 +11,17 @@ namespace Ambev.DeveloperEvaluation.Application.Sales.DeleteSale;
 public class DeleteSaleHandler : IRequestHandler<DeleteSaleCommand, DeleteSaleResult>
 {
     private readonly ISaleRepository _saleRepository;
+    private readonly ILogger<DeleteSaleHandler> _logger;
 
     /// <summary>
     /// Initializes a new instance of DeleteSaleHandler
     /// </summary>
     /// <param name="saleRepository">The sale repository</param>
-    public DeleteSaleHandler(ISaleRepository saleRepository)
+    /// <param name="logger">The Logger instance</param>
+    public DeleteSaleHandler(ISaleRepository saleRepository, ILogger<DeleteSaleHandler> logger)
     {
         _saleRepository = saleRepository;
+        _logger = logger;
     }
 
     /// <summary>
@@ -28,6 +33,8 @@ public class DeleteSaleHandler : IRequestHandler<DeleteSaleCommand, DeleteSaleRe
     /// <returns>The result of the delete operation</returns>    
     public async Task<DeleteSaleResult> Handle(DeleteSaleCommand request, CancellationToken cancellationToken)
     {
+        _logger.LogInformation("Cancelling Sale: {request}", JsonSerializer.Serialize(request));
+
         var sale = await _saleRepository.GetByIdAsync(request.Id, cancellationToken);
         
         if (sale is null)
@@ -36,6 +43,7 @@ public class DeleteSaleHandler : IRequestHandler<DeleteSaleCommand, DeleteSaleRe
         sale.Cancel();
         await _saleRepository.UpdateAsync(sale, cancellationToken);
 
+        _logger.LogInformation("Sale cancelled: {sale}", JsonSerializer.Serialize(sale));
         return new DeleteSaleResult(true);
     }
 }
